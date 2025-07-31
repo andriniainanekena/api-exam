038dec9f97cdbb9dfb7119f49a0afd9c70359354
Nekena Andriniaina <andriniainanekena@gmail.com>
2025-07-31 09:06:19 +0300

    add @app.exception_handler(404)

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ async def ping():
 @app.get("/home", response_class=HTMLResponse)
 async def home():
     return "<h1>Welcome home!</h1>"
+
+@app.exception_handler(404)
+async def not_found(request: Request, exc):
+    return HTMLResponse(content="<h1>404 NOT FOUND</h1>", status_code=404)
